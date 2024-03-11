c314fb6a68b6fbd864934ba347f5bf1be56f1874
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-03-12 01:34:26 +0600

    Create sidebar menu

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,13 +1,34 @@
+"use client"
+import { navLinks } from '@/constants'
+import { SignedIn } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = () => {
+    const pathname= usePathname()
   return (
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4'>
             <Link href='/' className='sidebar-logo'>
                 <Image src='/assets/images/NiN-Ai.svg' alt='logo' width={180} height={28} />
             </Link>
+            <nav className='sidebar-nav'>
+            <SignedIn>
+                <ul className='sidebar-nav_elements'>
+{navLinks.map(link=>{
+    const isActive=link.route===pathname
+    return(
+        <li key={link.route} className={`sidebar-nav_element group ${
+            isActive?'bg-purple-gradient text-white':'text-gray-700'
+        }}`}>
+{link?.label}
+        </li>
+    )
+})}
+                </ul>
+            </SignedIn>
+            </nav>
         </div>
     </aside>
   )
