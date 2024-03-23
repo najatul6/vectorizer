74d6d9db2ac5185f7a45b3688f39e530ab08e5d6
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-03-23 13:16:58 +0600

    Create preview picture

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import { useToast } from "@/components/ui/use-toast";
-import { CldUploadWidget } from "next-cloudinary";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 
 type MediaUploaderProps = {
@@ -19,6 +23,14 @@ const MediaUploader = ({
 }: MediaUploaderProps) => {
   const { toast } = useToast();
   const onUploadSuccessHandler = (result: any) => {
+    setImage((prevState:any)=>({
+        ...prevState,
+        publicId:result?.info?.public_id,
+        width:result?.info?.width,
+        height:result?.info?.height,
+        secureUrl:result?.info?.secure_url,
+    }))
+    onValueChange(result?.info?.public_id)
     toast({
       title: "Image uploaded successfully",
       description: "1 credit was deducted from your account",
@@ -48,7 +60,19 @@ const MediaUploader = ({
         <div className="flex flex-col gap-4">
           <h3 className="h3-bold text-dark-600">Original</h3>
           {publicId ? (
-            <>HERE IS THE IMAGE</>
+            <>
+              <div className="cursor-pointer overflow-hidden rounded-[10px] ">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="Image"
+                  sizes={"(max-width:767px)100vw,50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
+                />
+              </div>
+            </>
           ) : (
             <div className="media-uploader_cta" onClick={() => open()}>
               <div className="media-uploader_cta-image">
