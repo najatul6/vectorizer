7457037b44d33b4675b593ec37f52194ba1f1f80
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-03-28 00:27:39 +0600

    image delete and redirect

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -5,6 +5,7 @@ import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
+import { redirect } from "next/navigation";
 
 // ADD IMAGE
 export async function addImage({ image, userId, path }: AddImageParams) {
@@ -57,11 +58,11 @@ export async function updateImage({ image, userId, path }: UpdateImageParams) {
 export async function deleteImage(imageId:string ) {
   try {
     await connectToDatabase();
-    revalidatePath(path);
-
-    return JSON.parse(JSON.stringify(image));
+    await Image.findByIdAndDelete(imageId)
   } catch (error) {
     handleError(error);
+  }finally{
+    redirect('/')
   }
 }
 
@@ -69,6 +70,9 @@ export async function deleteImage(imageId:string ) {
 export async function getImageById(imageId: string) {
   try {
     await connectToDatabase();
+
+    const image = await
+
     revalidatePath(path);
 
     return JSON.parse(JSON.stringify(image));
