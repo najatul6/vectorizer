4834a14e76074ce2fb96e75b17c095f2793fc078
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-03-29 10:06:16 +0600

    getAllImages, add searchQuery,skipAmount,get totalImages,savedImages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import { Collection } from "@/components/shared/Collection";
 import { navLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
-const Home = () => {
+const Home = ({searchParams}:SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || ''
   return (
     <>
       <section className="home">
@@ -29,6 +32,10 @@ const Home = () => {
           </Link>
         ))}
       </ul>
+      </section>
+
+      <section className="sm:mt-12">
+          <Collection/>
       </section>
     </>
   );
