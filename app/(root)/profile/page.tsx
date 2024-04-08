7b01cff1019538c43ac7f98be4f8d66e839b027b
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-04-09 03:13:14 +0600

    change profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -18,7 +18,7 @@ const Profile = async ({ searchParams }: SearchParamProps) => {
 
   return (
     <>
-      <Header title="Profile" />
+      <Header title="Profile" subtitle="" />
 
       <section className="profile">
         <div className="profile-balance">
