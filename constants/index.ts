5affa6e23b673e5abc70d7377cbdc425dae5f4fc
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-04-09 02:48:42 +0600

    Create checkout page and edit constants

--- constants/index.ts
+++ constants/index.ts
@@ -47,7 +47,7 @@ export const navLinks = [
       name: "Free",
       icon: "/assets/icons/free-plan.svg",
       price: 0,
-      credits: 20,
+      credits: 10,
       inclusions: [
         {
           label: "20 Free Credits",
